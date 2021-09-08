a34570055c625c9df25b0c0016878047b946cf6e
govardhan <27.govardhan@gmail.com>
2021-09-08 19:30:49 +0530

    removed dist folder

--- packages/local-api/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serve = void 0;
-var serve = function (port, filename, dir) {
-    console.log('serving traffic on port ', port);
-    console.log('saving file ', filename);
-    console.log('directory ', dir);
-};
-exports.serve = serve;
